c2c6292b9cf4cc10e64e3233a26d08061d39508a
sharath-iiitl <99575424+sharath-iiitl@users.noreply.github.com>
2025-09-29 11:52:02 +0000

    nnnn

--- oracle-jet-3d/firstwebapp/src/js/viewModels/about.js
+++ oracle-jet-3d/firstwebapp/src/js/viewModels/about.js
@@ -8,12 +8,37 @@
 /*
  * Your about ViewModel code goes here
  */
-define(['../accUtils'],
- function(accUtils) {
+define(['../accUtils','knockout','ojs/ojchart'],
+ function(accUtils,ko) {
     function AboutViewModel() {
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
 
+            var self = this;
+
+      // ----------------------------------------------------------------
+
+      var data1 = [
+        {name:"Papers Published", items:[12]},
+        {name:"Projects Completed", items:[14]},
+        {name:"Team Size", items:[28]}   
+      ];
+
+      var data2 = [
+              {name:"Papers Published", items:[10]},
+              {name:"Projects Completed", items:[18]},
+              {name:"Team Size", items:[8]}   
+            ]; 
+      
+      var data3 = [
+              {name:"Papers Published", items:[17]},
+              {name:"Projects Completed", items:[8]},
+              {name:"Team Size", items:[18]}   
+            ];
+
+      self.datasource1 = ko.observableArray(data1);
+      self.datasource2 = ko.observableArray(data2);
+      self.datasource3 = ko.observableArray(data3);
       /**
        * Optional ViewModel method invoked after the View is inserted into the
        * document DOM.  The application can put logic that requires the DOM being
