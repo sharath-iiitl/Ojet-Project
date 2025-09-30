0ccd8527d3e523f32964058f73487103a26c3ea6
sharath-iiitl <99575424+sharath-iiitl@users.noreply.github.com>
2025-09-30 11:06:23 +0000

    n

--- oracle-jet-3d/firstwebapp/src/js/viewModels/incidents.js
+++ oracle-jet-3d/firstwebapp/src/js/viewModels/incidents.js
@@ -8,11 +8,48 @@
 /*
  * Your incidents ViewModel code goes here
  */
-define(['../accUtils'],
- function(accUtils) {
+define(['../accUtils','knockout', 'ojs/ojcollectiondataprovider', "ojs/ojmodel", "ojs/ojtable"],
+ function(accUtils, ko, CollectionDataProvider, Model, Table) {
     function IncidentsViewModel() {
       // Below are a set of the ViewModel methods invoked by the oj-module component.
       // Please reference the oj-module jsDoc for additional information.
+
+
+
+        var self = this;
+
+        // Setup the service URL
+        self.serviceURL = "https://apex.oracle.com/pls/apex/oraclejet/lp/activities/";
+
+        // Determine the structure/fields for the Model
+        self.parseData = function(response) {
+            return {name: response['name'], short_desc: response['short_desc']};
+        };
+
+        // Extend the model class for creating Department Model
+        self.Department = Model.Model.extend({
+            urlRoot: self.serviceURL,
+            parse: self.parseData,
+            idAttribute: 'id'
+        });
+
+        // We create a Model object
+        self.myDept = new self.Department();
+
+        // Extend the collection class 
+        self.DeptCollection = Model.Collection.extend({
+            url: self.serviceURL + "?limit=50",
+            model: self.myDept
+        });
+
+        // We create the collection object
+        self.DeptCol = ko.observable();
+        self.DeptCol(new self.DeptCollection());
+
+        // Connect the collection object to the table datasource
+        self.datasource = ko.observable();
+        self.datasource(new CollectionDataProvider(self.DeptCol()));
+
 
       /**
        * Optional ViewModel method invoked after the View is inserted into the
